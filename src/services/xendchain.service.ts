4a3c45922efe51734caf188ae5832d2237ea7718
Akintayo A. Olusegun <akintayo.segun@gmail.com>
2020-12-29 16:10:55 +0100

    send ngnc.
    Signed-off-by: Akintayo A. Olusegun <akintayo.segun@gmail.com>

--- src/services/xendchain.service.ts
+++ src/services/xendchain.service.ts
@@ -3,6 +3,8 @@ import { AES, enc } from "crypto-js";
 import Web3 from 'web3';
 import { Config } from "./config.service";
 import { Transaction, TxData } from 'ethereumjs-tx';
+import { AddressMapping } from "src/models/address.mapping.entity";
+import { EthereumService } from "./ethereum.service";
 
 @Injectable()
 export class XendChainService {
@@ -11,7 +13,7 @@ export class XendChainService {
     web3;
     erc20Abi;
 
-    constructor(private config: Config) {
+    constructor(private config: Config, private ethereumService: EthereumService) {
         this.web3 = new Web3(this.config.p["xendchain.server.url"]);
         this.erc20Abi = this.config.erc20Abi;
         this.ngncContractAddress = this.config.p["ngnc.contract.address"];
@@ -51,6 +53,33 @@ export class XendChainService {
 
                 const transaction = new Transaction(rawTransaction);
                 transaction.sign(xendPK);
+                const reciept = await this.web3.eth.sendSignedTransaction('0x' + transaction.serialize().toString('hex'))
+                resolve(reciept.transactionHash);
+            } catch (error) {
+                reject(error);
+            }
+        });
+    }
+
+    sendNgnc(sender: AddressMapping, recipient: string, amount: number): Promise<string> {
+        return new Promise(async (resolve, reject) => {
+            try {
+                const amountHex = this.web3.utils.toHex(amount);
+                const nonce: number = await this.web3.eth.getTransactionCount();
+                const contract = new this.web3.eth.Contract(this.erc20Abi, this.ngncContractAddress, { from: sender.chainAddress });
+
+                const block = await this.web3.eth.getBlock("latest");
+                var rawTransaction: TxData = {
+                    gasPrice: this.web3.utils.toHex(0),
+                    gasLimit: this.web3.utils.toHex(block.gasLimit),
+                    to: this.ngncContractAddress,
+                    value: "0x0",
+                    data: contract.methods.fundWallet(recipient, amountHex).encodeABI(),
+                    nonce: this.web3.utils.toHex(nonce),
+                }
+
+                const transaction = new Transaction(rawTransaction);
+                transaction.sign(this.ethereumService.getAddressFromEncryptedPK(sender.wif).privateKey);
                 const reciept = await this.web3.eth.sendSignedTransaction('0x' + transaction.serialize().toString('hex'))
                 resolve(reciept.transactionHash);
             } catch (error) {
